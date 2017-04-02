e5b6b9852086da23b1b3f54e2c952bd1811f9b3a
Ilia Gyrdymov <ilgyrd@gmail.com>
2017-04-02 23:02:51 +0300

    Example added  Please enter the commit message for your changes. Lines starting

--- example/linear_regression_example.dart
+++ example/linear_regression_example.dart
@@ -4,5 +4,14 @@
 import 'package:dart_ml/dart_ml.dart';
 
 main() {
+  LinearRegression predictor = new LinearRegression();
 
+  List<List<double>> features = [
+    [1.0, 2.0, 3.0, 223.3, 3.444, 23478.0],
+    [4.0, 5.0, 7.2, 309.1, 237.98, 2345.0]
+  ];
+
+  List<double> labels = [4.0, 3.0];
+
+  predictor.train(features, labels);
 }
