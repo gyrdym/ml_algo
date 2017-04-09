74ecdc82eba7acd4f9107c4b3b90ab6417580665
Ilia Gyrdymov <ilgyrd@gmail.com>
2017-04-09 12:54:24 +0300

    regular vector class refactored

--- lib/src/math/regular_vector.dart
+++ lib/src/math/regular_vector.dart
@@ -5,64 +5,28 @@ import 'package:dart_ml/src/enums.dart';
 import 'package:dart_ml/src/math/vector_interface.dart';
 
 class RegularVector extends ListBase<double> implements VectorInterface {
-  List<double> _innerList;
+  final List<double> _innerList;
 
-  RegularVector.fromList(List<double> source) {
-    _innerList = source;
-  }
+  RegularVector.fromList(List<double> this._innerList);
 
-  double operator [] (int index) => _innerList[index];
-
-  void operator []= (int index, double value) {
-    _innerList[index] = value;
-  }
-
-  RegularVector operator + (VectorInterface vector) {
-    List<double> _bufList = new List<double>(this.length);
-
-    for (int i = 0; i < this.length; i++) {
-      _bufList[i] = this[i] + vector[i];
-    }
-
-    return new RegularVector.fromList(_bufList);
-  }
-
-  RegularVector operator - (VectorInterface vector) {
-    List<double> _bufList = new List<double>(this.length);
-
-    for (int i = 0; i < this.length; i++) {
-      _bufList[i] = this[i] - vector[i];
-    }
-
-    return new RegularVector.fromList(_bufList);
-  }
-
-  RegularVector operator * (VectorInterface vector) {
-    List<double> _bufList = new List<double>(this.length);
-
-    for (int i = 0; i < this.length; i++) {
-      _bufList[i] = this[i] * vector[i];
-    }
-
-    return new RegularVector.fromList(_bufList);
-  }
-
-  RegularVector operator / (VectorInterface vector) {
-    List<double> _bufList = new List<double>(this.length);
-
-    for (int i = 0; i < this.length; i++) {
-      _bufList[i] = this[i] / vector[i];
-    }
-
-    return new RegularVector.fromList(_bufList);
-  }
-
-  void set length(int value) {
-    _innerList.length = value;
-  }
-
+  void set length(int value) {_innerList.length = value;}
   int get length => _innerList.length;
 
+  double operator [] (int index) => _innerList[index];
+  void operator []= (int index, double value) {_innerList[index] = value;}
+
+  RegularVector operator + (VectorInterface vector) => _elementWiseOperation(vector, (a,b) => a + b, false);
+  RegularVector operator - (VectorInterface vector) => _elementWiseOperation(vector, (a,b) => a - b, false);
+  RegularVector operator * (VectorInterface vector) => _elementWiseOperation(vector, (a,b) => a * b, false);
+  RegularVector operator / (VectorInterface vector) => _elementWiseOperation(vector, (a,b) => a / b, false);
+
+  RegularVector pow(double degree, {bool inPlace = false}) => _elementWiseOperation(degree, (a,b) => math.pow(a,b), inPlace);
+  RegularVector scalarAddition(double value, {bool inPlace = false}) => _elementWiseOperation(value, (a,b) => a + b, inPlace);
+  RegularVector scalarSubtraction(double value, {bool inPlace = false}) => _elementWiseOperation(value, (a,b) => a - b, inPlace);
+  RegularVector scalarMult(double value, {bool inPlace = false}) => _elementWiseOperation(value, (a,b) => a * b, inPlace);
+  RegularVector scalarDivision(double value, {bool inPlace = false}) => _elementWiseOperation(value, (a,b) => a / b, inPlace);
+
+  double vectorScalarMult(VectorInterface vector) => (this * vector)._sum();
   double distanceTo(VectorInterface vector, [Norm norm = Norm.EUCLIDEAN]) => (this - vector).norm(norm);
 
   double norm([Norm norm = Norm.EUCLIDEAN]) {
@@ -77,67 +41,15 @@ class RegularVector extends ListBase<double> implements VectorInterface {
     return math.pow(pow(degree)._sum(), 1 / degree);
   }
 
-  RegularVector pow(double degree, {bool inPlace = false}) {
-    List<double> _bufList;
-
-    if (inPlace == true) {
-      _bufList = _innerList;
-    } else {
-      _bufList = new List<double>(this.length);
-    }
-
-    for (int i = 0; i < this.length; i++) {
-      _bufList[i] = math.pow(this[i], degree);
-    }
-
-    if (inPlace == true) {
-      return this;
-    }
-
-    return new RegularVector.fromList(_bufList);
-  }
-
-  double vectorScalarMult(VectorInterface vector) => (this * vector)._sum();
-
-  RegularVector scalarMult(double value, {bool inPlace = false}) {
-    List<double> _bufList;
-
-    if (inPlace == true) {
-      _bufList = _innerList;
-    } else {
-      _bufList = new List<double>(this.length);
-    }
-
-    for (int i = 0; i < this.length; i++) {
-      _bufList[i] = this[i] * value;
-    }
-
-    if (inPlace == true) {
-      return this;
-    }
-
-    return new RegularVector.fromList(_bufList);
-  }
-
-  RegularVector scalarAddition(double value, {bool inPlace = false}) {
-    List<double> _bufList;
-
-    if (inPlace == true) {
-      _bufList = _innerList;
-    } else {
-      _bufList = new List<double>(this.length);
-    }
-
-    for (int i = 0; i < this.length; i++) {
-      _bufList[i] = this[i] + value;
-    }
-
-    if (inPlace == true) {
-      return this;
-    }
-
-    return new RegularVector.fromList(_bufList);
-  }
-
   double _sum() => this.reduce((double item, double sum) => item + sum);
+
+  RegularVector _elementWiseOperation(Object value, operation(double a, double b), bool inPlace) {
+    List<double> _bufList = inPlace ? _innerList : new List<double>(this.length);
+
+    for (int i = 0; i < this.length; i++) {
+      _bufList[i] = operation(this[i], (value is RegularVector ? value[i] : value));
+    }
+
+    return inPlace ? this : new RegularVector.fromList(_bufList);
+  }
 }
