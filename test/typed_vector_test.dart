d6902776fc2a069e34898a73c317461831693d60
Ilia Gyrdymov <ilgyrd@gmail.com>
2017-04-15 23:21:05 +0300

    test fixed

--- test/typed_vector_test.dart
+++ test/typed_vector_test.dart
@@ -1,6 +1,7 @@
 // Copyright (c) 2017, Ilya Gyrdymov. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
+import 'dart:typed_data';
 import 'package:dart_ml/dart_ml.dart' show TypedVector;
 import 'package:test/test.dart';
 import 'package:matcher/matcher.dart';
@@ -10,11 +11,14 @@ void main() {
   TypedVector vector2;
 
   group('Vector fundamental', () {
-    setUp(() {
-      vector1 = new TypedVector.from([1.0, 2.0, 3.0, 4.0, 5.0, 6.0]);
+    tearDown(() {
+      vector1 = null;
+      vector2 = null;
     });
 
-    test('Vector initialization: ', () {
+    test('Vector initialization via `from` constructor: ', () {
+      vector1 = new TypedVector.from([1.0, 2.0, 3.0, 4.0, 5.0, 6.0]);
+
       expect(vector1[0], equals(1.0));
       expect(vector1[4], equals(5.0));
       expect(vector1[5], equals(6.0));
@@ -24,11 +28,42 @@ void main() {
       expect(vector1.length, equals(6));
 
       vector2 = new TypedVector.from(new List.filled(11, 1.0));
+
       expect(vector2.length, 11);
       expect(vector2[10], 1.0);
     });
 
+    test('Vector initialization via `fromTypedList` constructor: ', () {
+      Float32x4List typedList = new Float32x4List.fromList([
+        new Float32x4(1.0, 2.0, 3.0, 4.0),
+        new Float32x4(5.0, 6.0, 7.0, 8.0),
+        new Float32x4(9.0, 10.0, 0.0, 0.0)
+      ]);
+
+      vector1 = new TypedVector.fromTypedList(typedList);
+
+      expect(vector1[0], equals(1.0));
+      expect(vector1[4], equals(5.0));
+      expect(vector1[5], equals(6.0));
+      expect(vector1[11], equals(0.0));
+      expect(() => vector1[12], throwsRangeError);
+      expect(() => vector1[-1], throwsRangeError);
+      expect(vector1.length, equals(12));
+
+      vector1 = new TypedVector.fromTypedList(typedList, 10);
+
+      expect(vector1[0], equals(1.0));
+      expect(vector1[4], equals(5.0));
+      expect(vector1[5], equals(6.0));
+      expect(vector1[9], equals(10.0));
+      expect(() => vector1[10], throwsRangeError);
+      expect(() => vector1[-1], throwsRangeError);
+      expect(vector1.length, equals(10));
+    });
+
     test('Vector elements updating: ', () {
+      vector1 = new TypedVector.from([1.0, 2.0, 3.0, 4.0, 5.0, 6.0]);
+
       vector1[0] = 34.0;
       vector1[4] = 277.0;
       vector1[5] = 1000.0;
@@ -39,6 +74,8 @@ void main() {
     });
 
     test('Vector length updating: ', () {
+      vector1 = new TypedVector.from([1.0, 2.0, 3.0, 4.0, 5.0, 6.0]);
+
       vector1.length = 8;
       expect(vector1.length, 8);
 
@@ -58,6 +95,11 @@ void main() {
     setUp(() {
       vector1 = new TypedVector.from([1.0, 2.0, 3.0, 4.0, 5.0]);
       vector2 = new TypedVector.from([1.0, 2.0, 3.0, 4.0, 5.0]);
+    });
+
+    tearDown(() {
+      vector1 = null;
+      vector2 = null;
     });
 
     test('Vector addition: ', () {
