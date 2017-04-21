374de1f0d585087242eadae1de53a61e2d94b41c
Ilia Gyrdymov <ilgyrd@gmail.com>
2017-04-21 23:37:30 +0300

    optimizer extracted

--- lib/src/predictors/predictor.dart
+++
@@ -1,7 +0,0 @@
-import 'package:dart_ml/src/math/vector_interface.dart';
-
-abstract class Predictor<T extends VectorInterface> {
-  void train(List<T> features, T labels);
-  T predict(List<T> features);
-  T get weights;
-}
--- lib/src/predictors/sgd_linear_regression.dart
+++ lib/src/predictors/sgd_linear_regression.dart
@@ -1,23 +1,23 @@
 import 'dart:mirrors';
 import 'dart:math' as math;
 import 'package:dart_ml/src/math/vector_interface.dart';
-import 'package:dart_ml/src/predictors/predictor.dart';
+import 'package:dart_ml/src/predictors/predictor_interface.dart';
+import 'package:dart_ml/src/optimizers/sgd_optimizer.dart';
 import 'package:dart_ml/src/enums.dart';
 
-class SGDLinearRegressor<T extends VectorInterface> implements Predictor<T> {
-  final double step;
-  final double minWeightsDistance;
-  final int iterationLimit;
-
+class SGDLinearRegressor<T extends VectorInterface> implements PredictorInterface<T> {
+  SGDOptimizer<T> _optimizer;
   T _weights;
 
   T get weights => _weights;
+  SGDOptimizer<T> get optimizer => _optimizer;
 
-  SGDLinearRegressor({this.step = 1e-5, this.minWeightsDistance = 1e-8, this.iterationLimit = 10000});
+  SGDLinearRegressor({double step = 1e-5, double minWeightsDistance = 1e-8, int iterationLimit = 10000})
+      : _optimizer = new SGDOptimizer(step: step, minWeightsDistance: minWeightsDistance, iterationLimit: iterationLimit);
 
   void train(List<T> features, T labels, [CostFunction metric = CostFunction.RMSE]) {
     _addBiasTo(features);
-    _weights = _optimize(features, labels, metric);
+    _weights = optimizer.optimize(features, labels, metric);
   }
 
   T predict(List<T> features) {
@@ -31,36 +31,5 @@ class SGDLinearRegressor<T extends VectorInterface> implements Predictor<T> {
 
   void _addBiasTo(List<T> features) {
     features.forEach((T vector) => vector.add(1.0));
-  }
-
-  T _optimize(List<T> features, T labels, CostFunction metric) {
-    math.Random randomizer = new math.Random();
-    double weightsDistance = double.MAX_FINITE;
-    int iterationCounter = 0;
-
-    T weights = (reflectType(T) as ClassMirror).newInstance(
-        const Symbol('filled'), [features.first.length, 0.0]).reflectee;
-
-    while (weightsDistance > minWeightsDistance && iterationCounter < iterationLimit) {
-      int k = randomizer.nextInt(features.length);
-      double eta = step / (iterationCounter + 1);
-      T newWeights = _doIteration(weights, features[k], labels[k], eta);
-      weightsDistance = newWeights.distanceTo(weights);
-      weights = newWeights;
-      iterationCounter++;
-    }
-
-    return weights;
-  }
-
-  T _doIteration(T weights, T features, double y, double eta) {
-    var newWeights = new List<double>.filled(features.length, 0.0);
-    var delta = weights.vectorScalarMult(features) - y;
-
-    for (int i = 0; i < features.length; i++) {
-      newWeights[i] = weights[i] - (2 * eta) * delta * features[i];
-    }
-
-    return (reflectType(T) as ClassMirror).newInstance(const Symbol('from'), [newWeights]).reflectee;
   }
 }
