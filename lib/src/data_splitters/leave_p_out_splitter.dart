17bf177a4a4941f34f1802f3c17b471247e07255
Ilia Gyrdymov <ilgyrd@gmail.com>
2017-05-14 12:34:41 +0300

    tests were added (1 fallen)

--- lib/src/data_splitters/leave_p_out_splitter.dart
+++ lib/src/data_splitters/leave_p_out_splitter.dart
@@ -3,12 +3,16 @@ import 'package:dart_ml/src/data_splitters/splitter_interface.dart';
 class LeavePOutSplitter implements SplitterInterface {
   final int _p;
 
-  LeavePOutSplitter({int p = 1}) : _p = p;
+  LeavePOutSplitter({int p = 2}) : _p = p {
+    if (p == 0 || p == 1) {
+      throw new UnsupportedError('Value `$p` for parameter `p` is unsupported');
+    }
+  }
 
   List<List<int>> split(int samplesLength) {
     List<List<int>> folds = <List<int>>[];
-    List<int> fold;
-    int counter;
+    List<int> fold = new List<int>(_p);
+    int counter = 1;
 
     void _initNewFold(int start) {
       fold = new List<int>(_p);
