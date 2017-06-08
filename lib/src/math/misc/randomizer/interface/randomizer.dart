e8787fdc69a2f3aa676543bef1ccc8f35ad0b530
Ilia Gyrdymov <ilgyrd@gmail.com>
2017-06-08 22:49:57 +0300

    randomizer class added

---
+++ lib/src/math/misc/randomizer/interface/randomizer.dart
@@ -0,0 +1,4 @@
+abstract class Randomizer {
+  int getIntegerFromInterval(int start, int end);
+  double getDoubleFromInterval(double start, double end);
+}
---
+++ test/gradient_descent_optimizer_test.dart
@@ -0,0 +1,1 @@
+import 'package:dart_ml/src/optimizer/gradient/optimizer.dart';
