324fd40093ad2246b047058265fe374cd98d50c7
Ilia Gyrdymov <ilgyrd@gmail.com>
2017-06-10 00:23:05 +0300

    norm exported

--- lib/src/math/math_impl.dart
+++ lib/src/math/math_impl.dart
@@ -1,2 +1,3 @@
 export 'package:dart_ml/src/math/misc/randomizer/implementation/randomizer.dart';
 export 'package:dart_ml/src/math/vector/vector.dart';
+export 'package:dart_ml/src/math/vector/norm.dart';
