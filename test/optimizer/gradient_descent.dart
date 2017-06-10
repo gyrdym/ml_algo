75524f6901ff4d9681081bbb85bad35395fa5806
Ilia Gyrdymov <ilgyrd@gmail.com>
2017-06-10 11:35:51 +0300

    dummy test fro batch gradient descent optimizer

--- test/optimizer/gradient_descent.dart
+++ test/optimizer/gradient_descent.dart
@@ -10,28 +10,41 @@ import 'package:dart_ml/src/optimizer/optimizer_impl.dart' show BGDOptimizerImpl
 class RandomizerMock extends Mock implements Randomizer {}
 
 void main() {
-  List<Vector> data = [
-    new Vector.from([230.1, 37.8, 69.2]),
-    new Vector.from([44.5, 39.3, 45.1]),
-    new Vector.from([17.2, 45.9, 69.3]),
-    new Vector.from([151.5, 41.3, 58.5]),
-    new Vector.from([180.8, 10.8, 58.4]),
-    new Vector.from([8.7, 48.9, 75.0]),
-    new Vector.from([57.5, 32.8, 23.5]),
-    new Vector.from([120.2, 19.6, 11.6]),
-    new Vector.from([8.6, 2.1, 1.0]),
-    new Vector.from([199.8, 2.6, 21.2])
-  ];
+  List<Vector> data;
+  Vector target;
 
-  Vector target = new Vector.from([22.1, 10.4, 9.3, 18.5, 12.9, 7.2, 11.8, 13.2, 4.8, 10.6]);
+  setUp(() {
+    injector = new ModuleInjector([
+      new Module()
+        ..bind(Randomizer, toFactory: () => new RandomizerMock())
+    ]);
 
-  injector = new ModuleInjector([
-    new Module()
-      ..bind(Randomizer, toFactory: () => new RandomizerMock())
-  ]);
+    data = [
+      new Vector.from([230.1, 37.8, 69.2]),
+      new Vector.from([44.5, 39.3, 45.1]),
+      new Vector.from([17.2, 45.9, 69.3]),
+      new Vector.from([151.5, 41.3, 58.5]),
+      new Vector.from([180.8, 10.8, 58.4]),
+      new Vector.from([8.7, 48.9, 75.0]),
+      new Vector.from([57.5, 32.8, 23.5]),
+      new Vector.from([120.2, 19.6, 11.6]),
+      new Vector.from([8.6, 2.1, 1.0]),
+      new Vector.from([199.8, 2.6, 21.2])
+    ];
 
-  group('Batch gradient descent', () {
-    BGDOptimizerImpl optimizer = new BGDOptimizerImpl();
-    optimizer.configure(1e-5, 1e-8, 10, Regularization.L2);
+    target = new Vector.from([22.1, 10.4, 9.3, 18.5, 12.9, 7.2, 11.8, 13.2, 4.8, 10.6]);
+  });
+
+  group('Batch gradient descent ', () {
+    BGDOptimizerImpl optimizer;
+
+    setUp(() {
+      optimizer = new BGDOptimizerImpl();
+      optimizer.configure(1e-5, 1e-8, 10, Regularization.L2);
+    });
+
+    test('should find optimal weights for the given data', () {
+      expect(optimizer.optimize(data, target), equals([]));
+    });
   });
 }
