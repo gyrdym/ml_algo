81bf19953fff51f6f35a5a2d16bfe0914bf892a8
Ilia Gyrdymov <ilgyrd@gmail.com>
2017-06-11 22:57:47 +0300

    refactored

--- lib/src/predictor/linear_regressor/gradient/implementation/stochastic.dart
+++ lib/src/predictor/linear_regressor/gradient/implementation/stochastic.dart
@@ -7,11 +7,11 @@ import 'base.dart';
 
 class SGDRegressor extends GradientLinearRegressor {
   SGDRegressor({double learningRate = 1e-5,
-                  double minWeightsDistance = 1e-8,
-                  int iterationLimit = 10000,
-                  EstimatorType estimatorType = EstimatorType.RMSE,
-                  Regularization regularization = Regularization.L2,
-                  alpha = .00001})
+                 double minWeightsDistance = 1e-8,
+                 int iterationLimit = 10000,
+                 EstimatorType estimatorType = EstimatorType.RMSE,
+                 Regularization regularization = Regularization.L2,
+                 alpha = .00001})
       : super(
       (injector.get(SGDOptimizer) as SGDOptimizer)
         ..configure(learningRate, minWeightsDistance, iterationLimit, regularization, alpha: alpha),
