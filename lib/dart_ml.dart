01c6c513eede01b162cd8053aba93aba8803ebd9
Ilia Gyrdymov <ilgyrd@gmail.com>
2017-06-11 23:46:08 +0300

    refactored

--- lib/dart_ml.dart
+++ lib/dart_ml.dart
@@ -6,7 +6,6 @@ export 'package:dart_ml/src/optimizer/regularization/regularization.dart';
 export 'package:dart_ml/src/estimator/rmse.dart';
 export 'package:dart_ml/src/estimator/mape.dart';
 export 'package:dart_ml/src/estimator/estimator_type.dart';
-export 'package:dart_ml/src/predictor/linear_regressor/linear_regressor.dart';
-export 'package:dart_ml/src/estimator/estimator_type.dart';
+export 'package:dart_ml/src/predictor/linear_regressor/linear_regressor.dart' show MBGDRegressor, BGDRegressor, SGDRegressor;
 export 'package:dart_ml/src/model_selection/validator/cross_validator_impl.dart';
 export 'package:dart_ml/src/di/di.dart';
