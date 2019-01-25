5a4e170484a4d896ede2da6981192e6ee1a0f452
Ilya Gyrdymov <ilgyrd@gmail.com>
2019-01-25 21:59:55 +0200

    unit tests added

--- test/data_preprocessing/ml_data/float32x4_csv_ml_data_with_categories_integration_test.dart
+++ test/data_preprocessing/ml_data/float32x4_csv_ml_data_with_categories_integration_test.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:typed_data';
 
+import 'package:ml_algo/categorical_data_encoder_type.dart';
 import 'package:ml_algo/src/data_preprocessing/ml_data/float32x4_csv_ml_data.dart';
 import 'package:ml_linalg/matrix.dart';
 import 'package:ml_linalg/vector.dart';
@@ -9,11 +10,23 @@ import 'package:tuple/tuple.dart';
 
 import '../../test_utils/helpers/floating_point_iterable_matchers.dart';
 
-Future testCsvWithCategories({String fileName, int labelIdx, int rowNum, Map<String, List<Object>> categories,
-  List<Tuple2<int, int>> columnsToRead,
+Future testCsvWithCategories({
+  String fileName,
+  bool headerExist = true,
+  int labelIdx,
+  int rowNum,
+  Map<String, List<Object>> categories,
+  List<Tuple2<int, int>> columns,
+  Map<String, CategoricalDataEncoderType> categoryNameToEncoder,
+  Map<int, CategoricalDataEncoderType> categoryIndexToEncoder,
   void testContentFn(MLMatrix<Float32x4> features, MLVector<Float32x4> labels, List<String> headers)}) async {
 
-  final data = Float32x4CsvMLDataInternal.fromFile(fileName, labelIdx: labelIdx, columns: columnsToRead,
+  final data = Float32x4CsvMLDataInternal.fromFile(fileName,
+      labelIdx: labelIdx,
+      columns: columns,
+      headerExists: headerExist,
+      categoryNameToEncoder: categoryNameToEncoder,
+      categoryIndexToEncoder: categoryIndexToEncoder,
       categories: categories);
   final header = await data.header;
   final features = await data.features;
@@ -27,12 +40,12 @@ Future testCsvWithCategories({String fileName, int labelIdx, int rowNum, Map<Str
 
 void main() {
   group('CsvMLData', () {
-    test('should encode categorical test_data via provided encoder', () async {
+    test('should encode data with help of predefined categories (`categories` parameter)', () async {
       await testCsvWithCategories(
           fileName: 'test/data_preprocessing/test_data/elo_blatter.csv',
           labelIdx: 1,
           rowNum: 209,
-          columnsToRead: [
+          columns: [
             const Tuple2(1, 7),
           ],
           categories: {
@@ -82,6 +95,94 @@ void main() {
               0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, // gdp_source
               0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, // popu_source
             ]));
+          }
+      );
+    });
+
+    test('should encode categorical data (`categoryNameToEncoder` parameter)', () async {
+      await testCsvWithCategories(
+          fileName: 'test/data_preprocessing/test_data/fake_data.csv',
+          labelIdx: 3,
+          rowNum: 7,
+          columns: [
+            const Tuple2(0, 3),
+          ],
+          categoryNameToEncoder: {
+            'feature_1': CategoricalDataEncoderType.oneHot,
+            'feature_2': CategoricalDataEncoderType.ordinal,
+            'feature_3': CategoricalDataEncoderType.oneHot,
+          },
+          testContentFn: (features, labels, header) {
+            expect(header, equals(['feature_1', 'feature_2', 'feature_3', 'score']));
+            expect(features, equals([
+              [1.0, 0.0, 0.0, /**/ 1.0, /**/ 1.0, 0.0, 0.0],
+              [0.0, 1.0, 0.0, /**/ 2.0, /**/ 0.0, 1.0, 0.0],
+              [0.0, 0.0, 1.0, /**/ 3.0, /**/ 0.0, 0.0, 1.0],
+              [0.0, 1.0, 0.0, /**/ 4.0, /**/ 1.0, 0.0, 0.0],
+              [0.0, 1.0, 0.0, /**/ 5.0, /**/ 0.0, 0.0, 1.0],
+              [1.0, 0.0, 0.0, /**/ 6.0, /**/ 0.0, 1.0, 0.0],
+              [0.0, 0.0, 1.0, /**/ 1.0, /**/ 0.0, 0.0, 1.0],
+            ]));
+            expect(labels, equals([1, 10, 200, 300, 400, 500, 700]));
+          }
+      );
+    });
+
+    test('should encode categorical data (`categorIndexToEncoder` parameter)', () async {
+      await testCsvWithCategories(
+          fileName: 'test/data_preprocessing/test_data/fake_data.csv',
+          labelIdx: 3,
+          rowNum: 7,
+          columns: [
+            const Tuple2(0, 3),
+          ],
+          categoryIndexToEncoder: {
+            0: CategoricalDataEncoderType.oneHot,
+            1: CategoricalDataEncoderType.ordinal,
+            2: CategoricalDataEncoderType.oneHot,
+          },
+          testContentFn: (features, labels, header) {
+            expect(header, equals(['feature_1', 'feature_2', 'feature_3', 'score']));
+            expect(features, equals([
+              [1.0, 0.0, 0.0, /**/ 1.0, /**/ 1.0, 0.0, 0.0],
+              [0.0, 1.0, 0.0, /**/ 2.0, /**/ 0.0, 1.0, 0.0],
+              [0.0, 0.0, 1.0, /**/ 3.0, /**/ 0.0, 0.0, 1.0],
+              [0.0, 1.0, 0.0, /**/ 4.0, /**/ 1.0, 0.0, 0.0],
+              [0.0, 1.0, 0.0, /**/ 5.0, /**/ 0.0, 0.0, 1.0],
+              [1.0, 0.0, 0.0, /**/ 6.0, /**/ 0.0, 1.0, 0.0],
+              [0.0, 0.0, 1.0, /**/ 1.0, /**/ 0.0, 0.0, 1.0],
+            ]));
+            expect(labels, equals([1, 10, 200, 300, 400, 500, 700]));
+          }
+      );
+    });
+
+    test('should encode categorical data in headless dataset', () async {
+      await testCsvWithCategories(
+          fileName: 'test/data_preprocessing/test_data/fake_data_headless.csv',
+          headerExist: false,
+          labelIdx: 3,
+          rowNum: 7,
+          columns: [
+            const Tuple2(0, 3),
+          ],
+          categoryIndexToEncoder: {
+            0: CategoricalDataEncoderType.oneHot,
+            1: CategoricalDataEncoderType.ordinal,
+            2: CategoricalDataEncoderType.oneHot,
+          },
+          testContentFn: (features, labels, header) {
+            expect(header, isNull);
+            expect(features, equals([
+              [1.0, 0.0, 0.0, /**/ 1.0, /**/ 1.0, 0.0, 0.0],
+              [0.0, 1.0, 0.0, /**/ 2.0, /**/ 0.0, 1.0, 0.0],
+              [0.0, 0.0, 1.0, /**/ 3.0, /**/ 0.0, 0.0, 1.0],
+              [0.0, 1.0, 0.0, /**/ 4.0, /**/ 1.0, 0.0, 0.0],
+              [0.0, 1.0, 0.0, /**/ 5.0, /**/ 0.0, 0.0, 1.0],
+              [1.0, 0.0, 0.0, /**/ 6.0, /**/ 0.0, 1.0, 0.0],
+              [0.0, 0.0, 1.0, /**/ 1.0, /**/ 0.0, 0.0, 1.0],
+            ]));
+            expect(labels, equals([1, 10, 200, 300, 400, 500, 700]));
           }
       );
     });
