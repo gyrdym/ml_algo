f25286753f609d9fa69c5a6de2b0889108bf4fdb
Ilya Gyrdymov <ilgyrd@gmail.com>
2019-06-29 10:47:21 +0300

    Decision tree leaf label factory (majority-based): probability calculation added for class label

--- lib/src/optimizer/non_linear/decision_tree/leaf_label_factory/majority_leaf_label_factory.dart
+++ lib/src/optimizer/non_linear/decision_tree/leaf_label_factory/majority_leaf_label_factory.dart
@@ -3,8 +3,9 @@ import 'package:ml_algo/src/optimizer/non_linear/decision_tree/leaf_label_factor
 import 'package:ml_algo/src/optimizer/non_linear/decision_tree/observations_distribution_counter/distribution_counter.dart';
 import 'package:ml_linalg/matrix.dart';
 import 'package:ml_linalg/vector.dart';
-import 'package:xrange/zrange.dart';
 import 'package:quiver/iterables.dart' as quiver_iterables;
+import 'package:tuple/tuple.dart';
+import 'package:xrange/zrange.dart';
 
 class MajorityLeafLabelFactory implements DecisionTreeLeafLabelFactory {
   MajorityLeafLabelFactory(this.distributionCounter);
@@ -15,22 +16,27 @@ class MajorityLeafLabelFactory implements DecisionTreeLeafLabelFactory {
   DecisionTreeLeafLabel create(Matrix observations, ZRange outcomesColumnRange,
       bool isClassLabelCategorical) {
     final outcomes = observations.submatrix(columns: outcomesColumnRange);
+    final totalRecordsCount = outcomes.rowsNum;
 
     if (isClassLabelCategorical) {
-      final leafLabelValue = _getLabelValue<Vector>(outcomes.rows);
-      return DecisionTreeLeafLabel.categorical(leafLabelValue);
+      final labelData = _getLabelData<Vector>(outcomes.rows, totalRecordsCount);
+      return DecisionTreeLeafLabel.categorical(labelData.item1,
+          probability: labelData.item2);
     }
 
-    final leafLabelValue = _getLabelValue<double>(outcomes.toVector());
-    return DecisionTreeLeafLabel.numerical(leafLabelValue);
+    final labelColumn = outcomes.toVector();
+    final labelData = _getLabelData<double>(labelColumn, totalRecordsCount);
+    return DecisionTreeLeafLabel.numerical(labelData.item1,
+        probability: labelData.item2);
   }
 
-  T _getLabelValue<T>(Iterable<T> values) {
+  Tuple2<T, double> _getLabelData<T>(Iterable<T> values, int totalCount) {
     final distribution = distributionCounter.count<T>(values);
-    final targetValue = quiver_iterables.max<MapEntry<T, int>>(
+    final targetLabelEntry = quiver_iterables.max<MapEntry<T, int>>(
       distribution.entries,
       (first, second) => first.value - second.value,
     );
-    return targetValue.key;
+    final probability = targetLabelEntry.value / totalCount;
+    return Tuple2(targetLabelEntry.key, probability);
   }
 }
