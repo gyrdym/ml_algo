d435045c41de8c1ff17b05de0f80c63528620c73
Ilya Gyrdymov <ilgyrd@gmail.com>
2019-09-12 00:52:53 +0300

    Decision tree: changelog and readme updated

--- lib/src/classifier/non_linear/decision_tree/decision_tree_classifier.dart
+++ lib/src/classifier/non_linear/decision_tree/decision_tree_classifier.dart
@@ -4,8 +4,16 @@ import 'package:ml_algo/src/model_selection/assessable.dart';
 import 'package:ml_algo/src/solver/non_linear/decision_tree/solver_factory/greedy_solver.dart';
 import 'package:ml_dataframe/ml_dataframe.dart';
 
+/// A factory that creates different presets of decision tree classifier
+///
+/// Decision tree is an algorithm that recursively splits the input data into
+/// subsets until the bests possible data subsets will be found.
 abstract class DecisionTreeClassifier implements Classifier, Assessable {
-  factory DecisionTreeClassifier(
+  /// Creates majority based decision tree classifier.
+  ///
+  /// Majority based decision tree - a simplest tree algorithm, that uses
+  /// majority label on a tree node as a prediction.
+  factory DecisionTreeClassifier.majority(
       DataFrame samples,
       {
         int targetId,
