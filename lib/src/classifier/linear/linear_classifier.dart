c8dba8a1b5156fa34c353a940b0f52d7044c77a7
Ilya Gyrdymov <ilgyrd@gmail.com>
2019-09-29 13:57:41 +0300

    linear classifier interface extended

--- lib/src/classifier/linear/linear_classifier.dart
+++ lib/src/classifier/linear/linear_classifier.dart
@@ -1,7 +1,14 @@
 import 'package:ml_algo/src/classifier/classifier.dart';
+import 'package:ml_algo/src/link_function/link_function.dart';
 import 'package:ml_linalg/matrix.dart';
 
 abstract class LinearClassifier implements Classifier {
+  LinkFunction get linkFunction;
+
+  bool get fitIntercept;
+
+  double get interceptScale;
+
   /// A matrix, where each column is a vector of coefficients, associated with
   /// the specific class
   Matrix get coefficientsByClasses;
--- lib/src/classifier/linear/linear_classifier_mixin.dart
+++ lib/src/classifier/linear/linear_classifier_mixin.dart
@@ -1,14 +1,9 @@
 import 'package:ml_algo/src/classifier/linear/linear_classifier.dart';
 import 'package:ml_algo/src/helpers/add_intercept_if.dart';
 import 'package:ml_algo/src/helpers/get_probabilities.dart';
-import 'package:ml_algo/src/link_function/link_function.dart';
 import 'package:ml_linalg/matrix.dart';
 
 mixin LinearClassifierMixin implements LinearClassifier {
-  LinkFunction get linkFunction;
-  bool get fitIntercept;
-  double get interceptScale;
-
   @override
   Matrix predictProbabilities(Matrix features) {
     final processedFeatures = addInterceptIf(fitIntercept, features,
