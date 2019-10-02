10c58a89231551d13f6d5b32196c63eac9e79b69
Ilya Gyrdymov <ilgyrd@gmail.com>
2019-10-03 00:02:32 +0300

    documentation for LogisticRegressor corrected and extended

--- lib/src/classifier/logistic_regressor.dart
+++ lib/src/classifier/logistic_regressor.dart
@@ -12,43 +12,52 @@ import 'package:ml_dataframe/ml_dataframe.dart';
 import 'package:ml_linalg/dtype.dart';
 import 'package:ml_linalg/matrix.dart';
 
-/// A factory that creates different presets of logistic regressors
+/// A class, performing logistic regression-based classification.
 ///
 /// Logistic regression is an algorithm that solves a binary classification
 /// problem. The algorithm uses maximization of the passed data likelihood.
 /// In other words, the regressor iteratively tries to select coefficients,
-/// that makes combination of passed features and these coefficients most
+/// that makes combination of passed features and their coefficients most
 /// likely.
 abstract class LogisticRegressor implements LinearClassifier, Assessable {
   /// Parameters:
   ///
   /// [fittingData] A [DataFrame] with observations, that will be used by the
   /// classifier to learn coefficients of the hyperplane, which divides the
-  /// features space, forming classes of the features. Should contain target
-  /// column id (index or name)
+  /// features space, forming clusters of positive and negative classes of the
+  /// observations. Must contain [targetName] column.
   ///
   /// [targetName] A string, that serves as a name of the target column (a
   /// column, that contains class labels or outcomes for the associated
-  /// features)
+  /// features).
+  ///
+  /// [optimizerType] Defines an algorithm of optimization, that will be used
+  /// to find the best coefficients of log-likelihood cost function. Also
+  /// defines, which regularization type (L1 or L2) one may use to learn a
+  /// logistic regressor.
   ///
   /// [iterationsLimit] A number of fitting iterations. Uses as a condition of
-  /// convergence in the solver. Default value is 100
+  /// convergence in the optimization algorithm. Default value is `100`.
   ///
   /// [initialLearningRate] A value, defining velocity of the convergence of the
-  /// gradient descent solver. Default value is 1e-3
+  /// gradient descent optimizer. Default value is `1e-3`.
   ///
-  /// [minCoefficientsUpdate] A minimum distance between weights vectors in two
-  /// subsequent iterations. Uses as a condition of convergence in the
-  /// solver. In other words, if difference is small, there is no reason to
-  /// continue fitting. Default value is 1e-12
+  /// [minCoefficientsUpdate] A minimum distance between coefficient vectors in
+  /// two subsequent iterations. Uses as a condition of convergence in the
+  /// optimization algorithm. If difference between the two vectors is small
+  /// enough, there is no reason to continue fitting. Default value is `1e-12`
   ///
   /// [probabilityThreshold] A probability, on the basis of which it is decided,
-  /// whether an observation relates to positive class label (label = 1) or
-  /// negative class label (label = 0). The greater the probability, the more
-  /// strict the classifier is. Default value is `0.5`
+  /// whether an observation relates to positive class label (see
+  /// [positiveLabel] parameter) or to negative class label (see [negativeLabel]
+  /// parameter). The greater the probability, the more strict the classifier
+  /// is. Default value is `0.5`.
   ///
-  /// [lambda] A coefficient of regularization. In gradient version of
-  /// logistic regression L2 regularization is used
+  /// [lambda] A coefficient of regularization. Uses to prevent the classifier
+  /// from overfitting. The more the value of [lambda], the more regular the
+  /// coefficients of log-likelihood cost function are. Extremely large [lambda]
+  /// may decrease the coefficients to nothing, otherwise too small [lambda] may
+  /// be a cause of too large absolute values of the coefficients.
   ///
   /// [randomSeed] A seed, that will be passed to a random value generator,
   /// used by stochastic optimizers. Will be ignored, if the solver is not
@@ -57,23 +66,42 @@ abstract class LogisticRegressor implements LinearClassifier, Assessable {
   /// receive a different result. To avoid it, define [randomSeed]
   ///
   /// [batchSize] A size of data (in rows), that will be used for fitting per
-  /// one iteration. If [batchSize] == `1` when stochastic gradient descent is
-  /// used; if `1` < [batchSize] < `total number of rows`, when mini-batch
-  /// gradient descent is used; if [batchSize] == `total number of rows`,
-  /// when full-batch gradient descent is used
+  /// one iteration. Applicable not for all optimizers. If gradient-based
+  /// optimizer uses and If [batchSize] == `1`, stochastic mode will be
+  /// activated; if `1` < [batchSize] < `total number of rows`, mini-batch mode
+  /// will be activated; if [batchSize] == `total number of rows`, full-batch
+  /// mode will be activated.
   ///
   /// [fitIntercept] Whether or not to fit intercept term. Default value is
-  /// `false`.
+  /// `false`. Intercept is a bias of the line (relative to X-axis) to be
+  /// learned by the classifier
   ///
-  /// [interceptScale] A value, defining a size of the intercept term
+  /// [interceptScale] A value, defining a size of the intercept.
+  ///
+  /// [isFittingDataNormalized] Defines, whether the [fittingData] normalized
+  /// or not. Normalization should be performed column-wise. Normalized data
+  /// may be needed for some optimizers (e.g., for
+  /// [LinearOptimizerType.vanillaCD])
   ///
   /// [learningRateType] A value, defining a strategy for the learning rate
   /// behaviour throughout the whole fitting process.
   ///
+  /// [initialCoefficientsType] Defines the coefficients for the first iteration
+  /// of optimization. By default, all the coefficients equal zeroes at the
+  /// start.
+  ///
+  /// [positiveLabel] Defines the value, that will be used for `positive` class.
+  /// By default, `1`.
+  ///
+  /// [negativeLabel] Defines the value, that will be used for `negative` class.
+  /// By default, `0`.
+  ///
   /// [dtype] A data type for all the numeric values, used by the algorithm. Can
   /// affect performance or accuracy of the computations. Default value is
   /// [DType.float32]
-  factory LogisticRegressor(DataFrame fittingData, String targetName, {
+  factory LogisticRegressor(
+      DataFrame fittingData,
+      String targetName, {
     LinearOptimizerType optimizerType = LinearOptimizerType.vanillaGD,
     int iterationsLimit = 100,
     double initialLearningRate = 1e-3,
