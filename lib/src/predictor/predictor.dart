a50ac392523323bb36e74099751df898f9651110
Ilya Gyrdymov <ilgyrd@gmail.com>
2019-10-04 22:01:19 +0300

    documentation for knn regressor added

--- lib/src/predictor/predictor.dart
+++ lib/src/predictor/predictor.dart
@@ -1,5 +1,6 @@
 import 'package:ml_dataframe/ml_dataframe.dart';
 
+/// A common interface for all types of classifiers and regressors
 abstract class Predictor {
   /// Returns prediction, based on the model learned parameters
   DataFrame predict(DataFrame features);
--- lib/src/regressor/knn_regressor.dart
+++ lib/src/regressor/knn_regressor.dart
@@ -7,32 +7,53 @@ import 'package:ml_dataframe/ml_dataframe.dart';
 import 'package:ml_linalg/distance.dart';
 import 'package:ml_linalg/dtype.dart';
 
-/// A factory for all the non parametric family of Machine Learning algorithms
+/// A class that performs regression basing on `k nearest neighbours` algorithm
+///
+/// K nearest neighbours algorithm is an algorithm that is targeted to search
+/// most similar labelled observations (number of these observations equals `k`)
+/// for the given unlabelled one.
+///
+/// In order to make a prediction, or rather to set a label for a given new
+/// observation, labels of found `k` observations are being summed up and
+/// divided by `k`.
+///
+/// To get a more precise result, one may use weighted average of found labels -
+/// the farther a found observation from the target one, the lower the weight of
+/// the observation is. To obtain these weights one may use a kernel function.
 abstract class KnnRegressor implements Assessable, Predictor {
-  /// Creates an instance of KNN regressor
+  /// Parameters:
   ///
-  /// KNN here means "K nearest neighbor"
+  /// [fittingData] Labelled observations, among which will be searched [k]
+  /// nearest neighbours for unlabelled observations. Must contain [targetName]
+  /// column.
   ///
-  /// [k] a number of nearest neighbours
+  /// [targetName] A string, that serves as a name of the column, that contains
+  /// labels (or outcomes).
   ///
-  /// [kernel] a type of kernel function, that will be used to find an outcome
-  /// for a new observation
+  /// [k] a number of nearest neighbours to be searched among [fittingData]
+  ///
+  /// [kernel] a type of a kernel function, that will be used to predict an
+  /// outcome for a new observation
   ///
   /// [distance] a distance type, that will be used to measure a distance
   /// between two observation vectors
+  ///
+  /// [dtype] A data type for all the numeric values, used by the algorithm. Can
+  /// affect performance or accuracy of the computations. Default value is
+  /// [DType.float32]
   factory KnnRegressor(DataFrame fittingData, String targetName, {
     int k,
     Kernel kernel = Kernel.uniform,
     Distance distance = Distance.euclidean,
     DType dtype = DType.float32,
   }) {
-    final featuresTargetSplits = featuresTargetSplit(fittingData,
+    final splits = featuresTargetSplit(fittingData,
       targetNames: [targetName],
     ).toList();
 
     return KnnRegressorImpl(
-      featuresTargetSplits[0].toMatrix(),
-      featuresTargetSplits[1].toMatrix(),
+      splits[0].toMatrix(),
+      splits[1].toMatrix(),
       targetName,
       k: k,
       kernel: kernel,
