6050899c2b7a7778eeb5c9c69e2ddf5fc4232535
Ilya Gyrdymov <ilgyrd@gmail.com>
2019-10-25 23:41:54 +0300

    test for knn classifier factory impl added

---
+++ test/classifier/knn_classifier/knn_classifier_factory_impl_test.dart
@@ -0,0 +1,22 @@
+import 'package:ml_algo/src/classifier/knn_classifier/knn_classifier_factory_impl.dart';
+import 'package:ml_algo/src/classifier/knn_classifier/knn_classifier_impl.dart';
+import 'package:ml_linalg/dtype.dart';
+import 'package:test/test.dart';
+
+import '../../mocks.dart';
+
+void main() {
+  group('KnnClassifierFactoryImpl', () {
+    test('should create a KnnClassifierImpl instance', () {
+      final factory = const KnnClassifierFactoryImpl();
+      final kernelMock = KernelMock();
+      final solverMock = KnnSolverMock();
+      final dtype = DType.float32;
+
+      final actual = factory.create('target', [1, 2, 3], kernelMock, solverMock,
+          dtype);
+
+      expect(actual, isA<KnnClassifierImpl>());
+    });
+  });
+}
