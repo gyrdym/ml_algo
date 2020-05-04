d43fa229e2990b8732e07f26a2cc156011cf277f
Ilya Gyrdymov <ilgyrd@gmail.com>
2020-05-04 23:02:48 +0300

    Unit tests for tree node json keys added

---
+++ test/tree_trainer/tree_node/tree_node_json_keys_test.dart
@@ -0,0 +1,30 @@
+import 'package:ml_algo/src/tree_trainer/tree_node/tree_node_json_keys.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Tree node json keys', () {
+    test('should contain json key for children field', () {
+      expect(childrenJsonKey, 'CN');
+    });
+
+    test('should contain json key for label field', () {
+      expect(labelJsonKey, 'LB');
+    });
+
+    test('should contain json key for splitting predicate type field', () {
+      expect(predicateTypeJsonKey, 'PT');
+    });
+
+    test('should contain json key for splitting value field', () {
+      expect(splittingValueJsonKey, 'SV');
+    });
+
+    test('should contain json key for splitting index field', () {
+      expect(splittingIndexJsonKey, 'SI');
+    });
+
+    test('should contain json key for level field', () {
+      expect(levelJsonKey, 'LV');
+    });
+  });
+}
