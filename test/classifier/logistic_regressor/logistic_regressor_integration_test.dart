026200196ef6c01d1a5e37fa3ed09423d18cdf06
Ilya Gyrdymov <ilgyrd@gmail.com>
2020-05-24 01:14:21 +0300

    softmax function fixed

--- test/classifier/logistic_regressor/logistic_regressor_integration_test.dart
+++ test/classifier/logistic_regressor/logistic_regressor_integration_test.dart
@@ -116,174 +116,6 @@ void main() {
       expect(score, equals(1.0));
     });
 
-    test('should consider intercept term', () {
-      final features = DataFrame(<Iterable<num>>[
-        [5.0, 7.0, 6.0, 1.0],
-        [1.0, 2.0, 3.0, 0.0],
-      ], headerExists: false);
-
-      final classifier = LogisticRegressor(
-        features,
-        targetName,
-        iterationsLimit: 1,
-        learningRateType: LearningRateType.constant,
-        initialLearningRate: 1.0,
-        batchSize: 2,
-        fitIntercept: true,
-      );
-      // as the intercept is required to be fitted, our test_data should look as follows:
-      //
-      // [5.0, 7.0, 6.0] => [1.0, 5.0, 7.0, 6.0]
-      // [1.0, 2.0, 3.0] => [1.0, 1.0, 2.0, 3.0]
-      //
-      // we add a new column to the test_data, the column is consisted of just ones, so we considered that our fitted line will
-      // start not right at the origin, but at the origin + some intercept. What is the value of the intercept? To answer
-      // this question, we have to find out the intercept's weight
-      //
-      // given test_data
-      // ----------------------------------------------
-      // | X (features):           | Y (class labels):|
-      // ---------------------------------------------|
-      // | [1.0, 5.0, 7.0, 6.0]    | [0.0]            |
-      // | [1.0, 1.0, 2.0, 3.0]    | [1.0]            |
-      // ----------------------------------------------
-      //
-      // formula for derivative:
-      // sum(x_i_j * (indicator(y=1) - P(y=1|x_i,w)))
-      //
-      // formula for the update:
-      // w_new = w_prev + eta * derivative (gradient ascent)
-      //
-      // ===============================================================================================================
-      // ITERATION 1, weights = [0.0, 0.0, 0.0, 0.0]
-      // ===============================================================================================================
-      // weights for class label 0.0:
-      // 1.0 * (1 - (1 / (1 + e^0))) = 0.5
-      // 5.0 * (1 - (1 / (1 + e^0))) = 2.5
-      // 7.0 * (1 - (1 / (1 + e^0))) = 3.5
-      // 6.0 * (1 - (1 / (1 + e^0))) = 3.0
-      //
-      // 1.0 * (0 - (1 / (1 + e^0))) = -0.5
-      // 1.0 * (0 - (1 / (1 + e^0))) = -0.5
-      // 2.0 * (0 - (1 / (1 + e^0))) = -1.0
-      // 3.0 * (0 - (1 / (1 + e^0))) = -1.5
-      //
-      // derivative: [0.0, 2.0, 2.5, 1.5]
-      // update: [0.0, 0.0, 0.0, 0.0] + 1.0 * [0.0, 2.0, 2.5, 1.5] = [0.0, 2.0, 2.5, 1.5]
-      //
-      // weights for class label 1.0:
-      // 1.0 * (0 - (1 / (1 + e^0))) = -0.5
-      // 5.0 * (0 - (1 / (1 + e^0))) = -2.5
-      // 7.0 * (0 - (1 / (1 + e^0))) = -3.5
-      // 6.0 * (0 - (1 / (1 + e^0))) = -3.0
-      //
-      // 1.0 * (1 - (1 / (1 + e^0))) = 0.5
-      // 1.0 * (1 - (1 / (1 + e^0))) = 0.5
-      // 2.0 * (1 - (1 / (1 + e^0))) = 1.0
-      // 3.0 * (1 - (1 / (1 + e^0))) = 1.5
-      //
-      // derivative: [0.0, -2.0, -2.5, -1.5]
-      // update: [0.0, 0.0, 0.0, 0.0] + 1.0 * [0.0, -2.0, -2.5, -1.5] = [0.0, -2.0, -2.5, -1.5]
-      expect(classifier.coefficientsByClasses, equals([
-        [0.0],
-        [2.0],
-        [2.5],
-        [1.5],
-      ]));
-    });
-
-    test('should consider intercept scale if intercept term is going to be '
-        'fitted', () {
-      final samples = DataFrame(<Iterable<num>>[
-        [5.0, 7.0, 6.0, 1.0],
-        [1.0, 2.0, 3.0, 0.0],
-        [3.0, 4.0, 5.0, 1.0],
-      ], headerExists: false);
-
-      final classifier = LogisticRegressor(
-        samples,
-        targetName,
-        iterationsLimit: 1,
-        learningRateType: LearningRateType.constant,
-        initialLearningRate: 1.0,
-        batchSize: 3,
-        fitIntercept: true,
-        interceptScale: 2.0,
-      );
-
-      // as the intercept is required to be fitted, our test_data should look as follows:
-      //
-      // [5.0, 7.0, 6.0] => [2.0, 5.0, 7.0, 6.0]
-      // [1.0, 2.0, 3.0] => [2.0, 1.0, 2.0, 3.0]
-      // [3.0, 4.0, 5.0] => [2.0, 3.0, 4.0, 5.0]
-      //
-      // we add a new column to the test_data, the column is consisted of just ones, so we considered that our fitted line will
-      // start not right from the origin, but from the origin + intercept equal to 2.0. What is the value of the intercept? To answer
-      // this question, we have to find out the intercept's weight
-      //
-      // given test_data
-      // ----------------------------------------------
-      // | X (features):           | Y (class labels):|
-      // ---------------------------------------------|
-      // | [2.0, 5.0, 7.0, 6.0]    | [0.0]            |
-      // | [2.0, 1.0, 2.0, 3.0]    | [1.0]            |
-      // | [2.0, 3.0, 4.0, 5.0]    | [0.0]            |
-      // ----------------------------------------------
-      //
-      // formula for derivative:
-      // sum(x_i_j * (indicator(y=1) - P(y=1|x_i,w)))
-      //
-      // formula for the update:
-      // w_new = w_prev + eta * derivative (gradient ascent)
-      //
-      // ===============================================================================================================
-      // ITERATION 1, weights = [0.0, 0.0, 0.0, 0.0]
-      // ===============================================================================================================
-      // weights for class label 0.0:
-      // 2.0 * (1 - (1 / (1 + e^0))) = 1.0
-      // 5.0 * (1 - (1 / (1 + e^0))) = 2.5
-      // 7.0 * (1 - (1 / (1 + e^0))) = 3.5
-      // 6.0 * (1 - (1 / (1 + e^0))) = 3.0
-      //
-      // 2.0 * (0 - (1 / (1 + e^0))) = -1.0
-      // 1.0 * (0 - (1 / (1 + e^0))) = -0.5
-      // 2.0 * (0 - (1 / (1 + e^0))) = -1.0
-      // 3.0 * (0 - (1 / (1 + e^0))) = -1.5
-      //
-      // 2.0 * (1 - (1 / (1 + e^0))) = 1.0
-      // 3.0 * (1 - (1 / (1 + e^0))) = 1.5
-      // 4.0 * (1 - (1 / (1 + e^0))) = 2.0
-      // 5.0 * (1 - (1 / (1 + e^0))) = 2.5
-      //
-      // derivative: [1.0, 3.5, 4.5, 4.0]
-      // update: [0.0, 0.0, 0.0, 0.0] + 1.0 * [1.0, 3.5, 4.5, 4.0] = [1.0, 3.5, 4.5, 4.0]
-      //
-      // weights for class label 1.0:
-      // 2.0 * (0 - (1 / (1 + e^0))) = -1.0
-      // 5.0 * (0 - (1 / (1 + e^0))) = -2.5
-      // 7.0 * (0 - (1 / (1 + e^0))) = -3.5
-      // 6.0 * (0 - (1 / (1 + e^0))) = -3.0
-      //
-      // 2.0 * (1 - (1 / (1 + e^0))) = 1.0
-      // 1.0 * (1 - (1 / (1 + e^0))) = 0.5
-      // 2.0 * (1 - (1 / (1 + e^0))) = 1.0
-      // 3.0 * (1 - (1 / (1 + e^0))) = 1.5
-      //
-      // 2.0 * (0 - (1 / (1 + e^0))) = -1.0
-      // 3.0 * (0 - (1 / (1 + e^0))) = -1.5
-      // 4.0 * (0 - (1 / (1 + e^0))) = -2.0
-      // 5.0 * (0 - (1 / (1 + e^0))) = -2.5
-      //
-      // derivative: [-1.0, -3.5, -4.5, -4.0]
-      // update: [0.0, 0.0, 0.0, 0.0] + 1.0 * [-1.0, -3.5, -4.5, -4.0] = [-1.0, -3.5, -4.5, -4.0]
-      expect(classifier.coefficientsByClasses, equals([
-        [1.0],
-        [3.5],
-        [4.5],
-        [4.0],
-      ]));
-    });
-
     group('serialization', () {
       final fitIntercept = false;
       final interceptScale = 3.0;
@@ -359,6 +191,208 @@ void main() {
         expect(restoredClassifier.linkFunction,
             isA<Float32InverseLogitLinkFunction>());
         expect(restoredClassifier.classNames, [targetName]);
+      });
+    });
+
+    group('fitIntercept', () {
+      final features = DataFrame(<Iterable<num>>[
+        [5.0, 7.0, 6.0, 1.0],
+        [1.0, 2.0, 3.0, 0.0],
+      ], headerExists: false);
+
+      final createClassifier = (DType dtype) => LogisticRegressor(
+        features,
+        targetName,
+        iterationsLimit: 1,
+        learningRateType: LearningRateType.constant,
+        initialLearningRate: 1.0,
+        batchSize: 2,
+        fitIntercept: true,
+        dtype: dtype,
+      );
+
+      test('should consider intercept term for float32 dtype case', () {
+        final classifier = createClassifier(DType.float32);
+
+        // as the intercept is required to be fitted, our test_data should look as follows:
+        //
+        // [5.0, 7.0, 6.0] => [1.0, 5.0, 7.0, 6.0]
+        // [1.0, 2.0, 3.0] => [1.0, 1.0, 2.0, 3.0]
+        //
+        // we add a new column to the test_data, the column is consisted of just ones, so we considered that our fitted line will
+        // start not right at the origin, but at the origin + some intercept. What is the value of the intercept? To answer
+        // this question, we have to find out the intercept's weight
+        //
+        // given test_data
+        // ----------------------------------------------
+        // | X (features):           | Y (class labels):|
+        // ---------------------------------------------|
+        // | [1.0, 5.0, 7.0, 6.0]    | [0.0]            |
+        // | [1.0, 1.0, 2.0, 3.0]    | [1.0]            |
+        // ----------------------------------------------
+        //
+        // formula for derivative:
+        // sum(x_i_j * (indicator(y=1) - P(y=1|x_i,w)))
+        //
+        // formula for the update:
+        // w_new = w_prev + eta * derivative (gradient ascent)
+        //
+        // ===============================================================================================================
+        // ITERATION 1, weights = [0.0, 0.0, 0.0, 0.0]
+        // ===============================================================================================================
+        // weights for class label 0.0:
+        // 1.0 * (1 - (1 / (1 + e^0))) = 0.5
+        // 5.0 * (1 - (1 / (1 + e^0))) = 2.5
+        // 7.0 * (1 - (1 / (1 + e^0))) = 3.5
+        // 6.0 * (1 - (1 / (1 + e^0))) = 3.0
+        //
+        // 1.0 * (0 - (1 / (1 + e^0))) = -0.5
+        // 1.0 * (0 - (1 / (1 + e^0))) = -0.5
+        // 2.0 * (0 - (1 / (1 + e^0))) = -1.0
+        // 3.0 * (0 - (1 / (1 + e^0))) = -1.5
+        //
+        // derivative: [0.0, 2.0, 2.5, 1.5]
+        // update: [0.0, 0.0, 0.0, 0.0] + 1.0 * [0.0, 2.0, 2.5, 1.5] = [0.0, 2.0, 2.5, 1.5]
+        //
+        // weights for class label 1.0:
+        // 1.0 * (0 - (1 / (1 + e^0))) = -0.5
+        // 5.0 * (0 - (1 / (1 + e^0))) = -2.5
+        // 7.0 * (0 - (1 / (1 + e^0))) = -3.5
+        // 6.0 * (0 - (1 / (1 + e^0))) = -3.0
+        //
+        // 1.0 * (1 - (1 / (1 + e^0))) = 0.5
+        // 1.0 * (1 - (1 / (1 + e^0))) = 0.5
+        // 2.0 * (1 - (1 / (1 + e^0))) = 1.0
+        // 3.0 * (1 - (1 / (1 + e^0))) = 1.5
+        //
+        // derivative: [0.0, -2.0, -2.5, -1.5]
+        // update: [0.0, 0.0, 0.0, 0.0] + 1.0 * [0.0, -2.0, -2.5, -1.5] = [0.0, -2.0, -2.5, -1.5]
+        expect(classifier.coefficientsByClasses, equals([
+          [0.0],
+          [2.0],
+          [2.5],
+          [1.5],
+        ]));
+      });
+
+      test('should consider intercept term for float64 dtype case', () {
+        final classifier = createClassifier(DType.float64);
+
+        expect(classifier.coefficientsByClasses, equals([
+          [0.0],
+          [2.0],
+          [2.5],
+          [1.5],
+        ]));
+      });
+    });
+
+    group('interceptScale', () {
+      final samples = DataFrame(<Iterable<num>>[
+        [5.0, 7.0, 6.0, 1.0],
+        [1.0, 2.0, 3.0, 0.0],
+        [3.0, 4.0, 5.0, 1.0],
+      ], headerExists: false);
+
+      final createClassifier = (DType dtype) => LogisticRegressor(
+        samples,
+        targetName,
+        iterationsLimit: 1,
+        learningRateType: LearningRateType.constant,
+        initialLearningRate: 1.0,
+        batchSize: 3,
+        fitIntercept: true,
+        interceptScale: 2.0,
+        dtype: dtype,
+      );
+
+      test('should consider intercept scale if intercept term is going to be '
+          'fitted for float32 case', () {
+        final classifier = createClassifier(DType.float32);
+
+        // as the intercept is required to be fitted, our test_data should look as follows:
+        //
+        // [5.0, 7.0, 6.0] => [2.0, 5.0, 7.0, 6.0]
+        // [1.0, 2.0, 3.0] => [2.0, 1.0, 2.0, 3.0]
+        // [3.0, 4.0, 5.0] => [2.0, 3.0, 4.0, 5.0]
+        //
+        // we add a new column to the test_data, the column is consisted of just ones, so we considered that our fitted line will
+        // start not right from the origin, but from the origin + intercept equal to 2.0. What is the value of the intercept? To answer
+        // this question, we have to find out the intercept's weight
+        //
+        // given test_data
+        // ----------------------------------------------
+        // | X (features):           | Y (class labels):|
+        // ---------------------------------------------|
+        // | [2.0, 5.0, 7.0, 6.0]    | [0.0]            |
+        // | [2.0, 1.0, 2.0, 3.0]    | [1.0]            |
+        // | [2.0, 3.0, 4.0, 5.0]    | [0.0]            |
+        // ----------------------------------------------
+        //
+        // formula for derivative:
+        // sum(x_i_j * (indicator(y=1) - P(y=1|x_i,w)))
+        //
+        // formula for the update:
+        // w_new = w_prev + eta * derivative (gradient ascent)
+        //
+        // ===============================================================================================================
+        // ITERATION 1, weights = [0.0, 0.0, 0.0, 0.0]
+        // ===============================================================================================================
+        // weights for class label 0.0:
+        // 2.0 * (1 - (1 / (1 + e^0))) = 1.0
+        // 5.0 * (1 - (1 / (1 + e^0))) = 2.5
+        // 7.0 * (1 - (1 / (1 + e^0))) = 3.5
+        // 6.0 * (1 - (1 / (1 + e^0))) = 3.0
+        //
+        // 2.0 * (0 - (1 / (1 + e^0))) = -1.0
+        // 1.0 * (0 - (1 / (1 + e^0))) = -0.5
+        // 2.0 * (0 - (1 / (1 + e^0))) = -1.0
+        // 3.0 * (0 - (1 / (1 + e^0))) = -1.5
+        //
+        // 2.0 * (1 - (1 / (1 + e^0))) = 1.0
+        // 3.0 * (1 - (1 / (1 + e^0))) = 1.5
+        // 4.0 * (1 - (1 / (1 + e^0))) = 2.0
+        // 5.0 * (1 - (1 / (1 + e^0))) = 2.5
+        //
+        // derivative: [1.0, 3.5, 4.5, 4.0]
+        // update: [0.0, 0.0, 0.0, 0.0] + 1.0 * [1.0, 3.5, 4.5, 4.0] = [1.0, 3.5, 4.5, 4.0]
+        //
+        // weights for class label 1.0:
+        // 2.0 * (0 - (1 / (1 + e^0))) = -1.0
+        // 5.0 * (0 - (1 / (1 + e^0))) = -2.5
+        // 7.0 * (0 - (1 / (1 + e^0))) = -3.5
+        // 6.0 * (0 - (1 / (1 + e^0))) = -3.0
+        //
+        // 2.0 * (1 - (1 / (1 + e^0))) = 1.0
+        // 1.0 * (1 - (1 / (1 + e^0))) = 0.5
+        // 2.0 * (1 - (1 / (1 + e^0))) = 1.0
+        // 3.0 * (1 - (1 / (1 + e^0))) = 1.5
+        //
+        // 2.0 * (0 - (1 / (1 + e^0))) = -1.0
+        // 3.0 * (0 - (1 / (1 + e^0))) = -1.5
+        // 4.0 * (0 - (1 / (1 + e^0))) = -2.0
+        // 5.0 * (0 - (1 / (1 + e^0))) = -2.5
+        //
+        // derivative: [-1.0, -3.5, -4.5, -4.0]
+        // update: [0.0, 0.0, 0.0, 0.0] + 1.0 * [-1.0, -3.5, -4.5, -4.0] = [-1.0, -3.5, -4.5, -4.0]
+        expect(classifier.coefficientsByClasses, equals([
+          [1.0],
+          [3.5],
+          [4.5],
+          [4.0],
+        ]));
+      });
+
+      test('should consider intercept scale if intercept term is going to be '
+          'fitted for float32 case', () {
+        final classifier = createClassifier(DType.float64);
+
+        expect(classifier.coefficientsByClasses, equals([
+          [1.0],
+          [3.5],
+          [4.5],
+          [4.0],
+        ]));
       });
     });
   });
