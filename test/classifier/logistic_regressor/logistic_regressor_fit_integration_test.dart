7ca52002408bddb30a0de40dffa08ccef97c27d9
Ilya Gyrdymov <ilgyrd@gmail.com>
2020-05-24 15:03:44 +0300

    logistic regressor unit tests restructured

--- test/classifier/logistic_regressor/logistic_regressor_fit_integration_test.dart
+++ test/classifier/logistic_regressor/logistic_regressor_fit_integration_test.dart
@@ -1,114 +1,63 @@
 import 'package:ml_algo/src/classifier/logistic_regressor/logistic_regressor.dart';
 import 'package:ml_algo/src/di/injector.dart';
 import 'package:ml_algo/src/linear_optimizer/gradient_optimizer/learning_rate_generator/learning_rate_type.dart';
-import 'package:ml_algo/src/metric/metric_type.dart';
 import 'package:ml_dataframe/ml_dataframe.dart';
+import 'package:ml_linalg/dtype.dart';
 import 'package:ml_linalg/linalg.dart';
-import 'package:ml_linalg/matrix.dart';
 import 'package:ml_tech/unit_testing/matchers/iterable_2d_almost_equal_to.dart';
 import 'package:test/test.dart';
 
 void main() {
   group('LogisticRegressor', () {
-    final data = <Iterable<num>>[
-      [5.0, 7.0, 6.0, 1.0],
-      [1.0, 2.0, 3.0, 0.0],
-      [10.0, 12.0, 31.0, 0.0],
-      [9.0, 8.0, 5.0, 0.0],
-      [4.0, 0.0, 1.0, 1.0],
-    ];
-    final targetName = 'col_3';
-    final samples = DataFrame(data, headerExists: false);
+    group('fit', () {
+      final data = <Iterable<num>>[
+        [5.0, 7.0, 6.0, 1.0],
+        [1.0, 2.0, 3.0, 0.0],
+        [10.0, 12.0, 31.0, 0.0],
+        [9.0, 8.0, 5.0, 0.0],
+        [4.0, 0.0, 1.0, 1.0],
+      ];
+      final targetName = 'col_3';
+      final samples = DataFrame(data, headerExists: false);
 
-    tearDownAll(() => injector = null);
+      tearDownAll(() => injector = null);
 
-    test('should fit given data', () {
-      final classifier = LogisticRegressor(
-        samples,
-        targetName,
-        iterationsLimit: 2,
-        learningRateType: LearningRateType.constant,
-        initialLearningRate: 1.0,
-        batchSize: 5,
-        fitIntercept: false,
-      );
-
-      expect(classifier.coefficientsByClasses, iterable2dAlmostEqualTo([
-        [3.5,],
-        [-0.5,],
-        [-9.0,],
-      ], 1e-2));
-    });
-
-    test('should make prediction', () {
-      final classifier = LogisticRegressor(
-        samples,
-        targetName,
-        iterationsLimit: 2,
-        learningRateType: LearningRateType.constant,
-        initialLearningRate: 1.0,
-        batchSize: 5,
-        fitIntercept: false,
-      );
-
-      final newFeatures = Matrix.fromList([
-        [2.0, 4.0, 1.0],
-      ]);
-
-      final probabilities = classifier.predictProbabilities(
-        DataFrame.fromMatrix(newFeatures),
-      );
-
-      final classes = classifier.predict(
-        DataFrame.fromMatrix(newFeatures),
-      );
-
-      expect(probabilities.header, equals(['col_3']));
-      expect(probabilities.toMatrix(), equals([[0.01798621006309986]]));
-      expect(classes.header, equals(['col_3']));
-      expect(classes.toMatrix(), equals([[0.0]]));
-    });
-
-    test('should evaluate prediction quality, accuracy = 0', () {
-      final classifier = LogisticRegressor(
+      test('should fit given data, float32 case', () {
+        final classifier = LogisticRegressor(
           samples,
           targetName,
           iterationsLimit: 2,
           learningRateType: LearningRateType.constant,
           initialLearningRate: 1.0,
           batchSize: 5,
-          fitIntercept: false
-      );
+          fitIntercept: false,
+        );
 
-      final newSamples = DataFrame([
-        <num>[2.0, 4.0, 1.0, 1.0],
-      ], header: ['first', 'second', 'third', 'target'], headerExists: false);
+        expect(classifier.coefficientsByClasses, iterable2dAlmostEqualTo([
+          [3.5,],
+          [-0.5,],
+          [-9.0,],
+        ], 1e-2));
+      });
 
-      final score = classifier.assess(newSamples, ['target'],
-          MetricType.accuracy);
-
-      expect(score, equals(0.0));
-    });
-
-    test('should evaluate prediction quality, accuracy = 1', () {
-      final classifier = LogisticRegressor(
+      test('should fit given data, float64 case', () {
+        final classifier = LogisticRegressor(
           samples,
           targetName,
           iterationsLimit: 2,
           learningRateType: LearningRateType.constant,
           initialLearningRate: 1.0,
           batchSize: 5,
-          fitIntercept: false
-      );
+          fitIntercept: false,
+          dtype: DType.float64,
+        );
 
-      final newFeatures = DataFrame([
-        <num>[2, 4, 1, 0],
-      ], header: ['first', 'second', 'third', 'target'], headerExists: false);
-
-      final score = classifier.assess(newFeatures, ['target'],
-          MetricType.accuracy);
-
-      expect(score, equals(1.0));
+        expect(classifier.coefficientsByClasses, iterable2dAlmostEqualTo([
+          [3.5,],
+          [-0.5,],
+          [-9.0,],
+        ], 1e-2));
+      });
     });
 
     group('fitIntercept', () {
@@ -116,6 +65,7 @@ void main() {
         [5.0, 7.0, 6.0, 1.0],
         [1.0, 2.0, 3.0, 0.0],
       ], headerExists: false);
+      final targetName = 'col_3';
 
       final createClassifier = (DType dtype) => LogisticRegressor(
         features,
@@ -210,6 +160,7 @@ void main() {
         [1.0, 2.0, 3.0, 0.0],
         [3.0, 4.0, 5.0, 1.0],
       ], headerExists: false);
+      final targetName = 'col_3';
 
       final createClassifier = (DType dtype) => LogisticRegressor(
         samples,
