f5aefb3ba616ee4293b5b007dc8113b726c19ee2
Ilya Gyrdymov <ilgyrd@gmail.com>
2020-05-24 17:55:34 +0300

    tests structure reorganized

--- test/link_function/helpers/from_link_function_json_test.dart
+++ test/link_function/helpers/from_link_function_json_test.dart
@@ -1,7 +1,9 @@
 import 'package:ml_algo/src/link_function/helpers/from_link_function_json.dart';
 import 'package:ml_algo/src/link_function/link_function_encoded_values.dart';
 import 'package:ml_algo/src/link_function/logit/float32_inverse_logit_function.dart';
+import 'package:ml_algo/src/link_function/logit/float64_inverse_logit_function.dart';
 import 'package:ml_algo/src/link_function/softmax/float32_softmax_link_function.dart';
+import 'package:ml_algo/src/link_function/softmax/float64_softmax_link_function.dart';
 import 'package:test/test.dart';
 
 void main() {
@@ -13,9 +15,22 @@ void main() {
       expect(decoded, same(logitFunction));
     });
 
+    test('should decode Float64 based inverse logit link function', () {
+      final logitFunction = const Float64InverseLogitLinkFunction();
+      final decoded = fromLinkFunctionJson(
+          float64InverseLogitLinkFunctionEncoded);
+      expect(decoded, same(logitFunction));
+    });
+
     test('should decode Float32 based softmax link function', () {
       final softmaxFunction = const Float32SoftmaxLinkFunction();
       final decoded = fromLinkFunctionJson(float32SoftmaxLinkFunctionEncoded);
+      expect(decoded, same(softmaxFunction));
+    });
+
+    test('should decode Float64 based softmax link function', () {
+      final softmaxFunction = const Float64SoftmaxLinkFunction();
+      final decoded = fromLinkFunctionJson(float64SoftmaxLinkFunctionEncoded);
       expect(decoded, same(softmaxFunction));
     });
 
