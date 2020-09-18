b32fb734291fa2ab6d858c05a23321fc3c6d703a
Ilya Gyrdymov <ilgyrd@gmail.com>
2020-09-19 01:03:16 +0300

    RSS metric added

--- lib/src/metric/regression/rss.dart
+++ lib/src/metric/regression/rss.dart
@@ -1,22 +1,21 @@
-import 'dart:math' as math;
-
 import 'package:ml_algo/src/helpers/validate_matrix_columns.dart';
 import 'package:ml_algo/src/metric/metric.dart';
-import 'package:ml_linalg/linalg.dart';
+import 'package:ml_linalg/matrix.dart';
 
-class RmseMetric implements Metric {
-  const RmseMetric();
+class RssMetric implements Metric {
+  const RssMetric();
 
   @override
   double getScore(Matrix predictedLabels, Matrix origLabels) {
     validateMatrixColumns([predictedLabels, origLabels]);
 
     final predicted = predictedLabels
-        .getColumn(0);
+        .toVector();
     final original = origLabels
-        .getColumn(0);
+        .toVector();
 
-    return math
-        .sqrt(((predicted - original).pow(2)).mean());
+    return (predicted - original)
+        .pow(2)
+        .sum();
   }
 }
