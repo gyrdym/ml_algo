c0ffc71676c1ad14927448fe9bbf984a425ce27a
Ilya Gyrdymov <ilgyrd@gmail.com>
2022-03-07 01:25:03 +0200

    Added LinearRegressor.SGD constructor

--- lib/src/regressor/linear_regressor/linear_regressor.dart
+++ lib/src/regressor/linear_regressor/linear_regressor.dart
@@ -249,7 +249,8 @@ abstract class LinearRegressor
             dtype: dtype,
           );
 
-  /// Linear regression with Stochastic Gradient Descent optimization
+  /// Linear regression with Stochastic Gradient Descent optimization with L2
+  /// regularization
   ///
   /// Parameters:
   ///
@@ -280,12 +281,11 @@ abstract class LinearRegressor
   /// optimization algorithm. If difference between the two vectors is small
   /// enough, there is no reason to continue fitting. Default value is `1e-12`
   ///
-  /// [lambda] A coefficient of regularization. Uses to prevent the regressor's
+  /// [lambda] L2 regularization coefficient. Uses to prevent the regressor's
   /// overfitting. The greater the value of [lambda], the more regular the
-  /// coefficients of the equation of the predicting hyperplane are. Extremely
-  /// large [lambda] may decrease the coefficients to nothing, otherwise too
-  /// small [lambda] may be a cause of too large absolute values of the
-  /// coefficients.
+  /// coefficients are. Extremely large [lambda] may decrease the coefficients
+  /// to nothing, otherwise too small [lambda] may be a cause of too large
+  /// absolute values of the coefficients.
   ///
   /// [randomSeed] A seed value that will be passed to a random value generator,
   /// used by stochastic optimizers. Will be ignored, if the solver is not
